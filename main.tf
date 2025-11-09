4b24cb382ef0943be3c4c62384863f29ed6797e2
sean-hansen-sr <sean.hansen.sr@gmail.com>
2025-11-09 08:54:38 -0600

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "qwiklabs-gcp-03-dec96dbe6cce"
+  zone    = "us-east1-b"
+}
+
+resource "google_compute_instance" "default" {
+  name         = "terraform"
+  machine_type = "e2-medium"
+  zone         = "us-east1-b"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+    }
+  }
